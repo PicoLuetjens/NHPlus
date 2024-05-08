107b6c33cddc6a9f4fb501e1ee651b61f604745e
Kevin Schmidt <kevin.schmidt@hmmh.de>
2024-05-08 14:04:00 +0200

    NHPlus-7/ cleanup imports in main, add nurseview in treatments fxml

--- src/main/java/de/hitec/nhplus/Main.java
+++ src/main/java/de/hitec/nhplus/Main.java
@@ -1,17 +1,8 @@
 package de.hitec.nhplus;
 
 import de.hitec.nhplus.application.LoginForm;
-import de.hitec.nhplus.application.MainWindow;
-import de.hitec.nhplus.datastorage.ConnectionBuilder;
 import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.application.Preloader;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class Main extends Application {
 	private Stage primaryStage;
