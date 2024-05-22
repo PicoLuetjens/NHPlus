85ee3ba76b7709937418d21275ab85eb0936323b
Kevin Schmidt <kevin.schmidt@hmmh.de>
2024-05-22 10:41:38 +0200

    NHPlus-21/ refactor main

--- src/main/java/de/hitec/nhplus/Main.java
+++ src/main/java/de/hitec/nhplus/Main.java
@@ -9,13 +9,13 @@ import java.net.URL;
 
 public class Main extends Application {
 	private Stage primaryStage;
+
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		this.primaryStage = primaryStage;
 		LoginForm login = new LoginForm();
 		login.start(primaryStage);
 	}
-
 
 	public static void main(String[] args) {
 
@@ -25,15 +25,11 @@ public class Main extends Application {
 		final Taskbar taskbar = Taskbar.getTaskbar();
 		try {
 			taskbar.setIconImage(image);
-		}
-		catch (final UnsupportedOperationException e) {
+		} catch (final UnsupportedOperationException e) {
 			System.out.println("The os does not support: 'taskbar.setIconImage'");
-		}
-		catch (final SecurityException e) {
+		} catch (final SecurityException e) {
 			System.out.println("There was a security exception for: 'taskbar.setIconImage'");
 		}
-
 		launch(args);
 	}
-
 }
